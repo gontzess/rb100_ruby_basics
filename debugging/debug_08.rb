859b1abcfe62151ec73a49b8c540de64ceeb52ad
Stephen Gontzes <gontzess@gmail.com>
2020-12-29 22:12:38 -0500

    debugging exercises

---
+++ debugging/debug_08.rb
@@ -0,0 +1,22 @@
+# colorful things
+
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+length = [colors.length, things.length].min
+
+loop do
+  break if i >= length
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+
+  i += 1
+end
